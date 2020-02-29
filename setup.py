dc183ca7f7ebf2372f86a2439551c27484560ebd
Oscar Beijbom <oscar.beijbom@gmail.com>
2020-02-28 17:54:33 -0800

    updated install_requirements. Narrowed down support to python 3.5 and 3.6

--- setup.py
+++ setup.py
@@ -18,17 +18,16 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='==3.5.*',
+    python_requires='>=3.5, <3.7',
     install_requires=[
         'boto=2.49.0',
         'wget=3.2',
+        'tqdm==4.43.0',
+        'Pillow==6.2.0',
+        'numpy=1.17.5',
+        'scipy=1.2.3',
         'scikit-learn=0.17.1',
         'scikit-image=0.15.0',
-        'scipy=0.19.1',
-        'numpy=1.17.1',
-        'Pillow==6.2.0',
-        'tqdm==4.43.0',
-        'fire==0.2.1',
         'torch==1.4.0',
         'torchvision==0.5.0'
     ]
