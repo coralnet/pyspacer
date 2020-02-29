e809c3c7d74869fb7b0f0ac83ac440a4d4c0d24b
Oscar Beijbom <oscar.beijbom@gmail.com>
2020-02-28 17:26:04 -0800

    updated numpy to 1.17.5 and scipy to 1.2.3

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="pyspacer-BEIJBOM",  # Replace with your own username
-    version="1.0.0",
+    name="pyspacer-BEIJBOMM",  # Replace with your own username
+    version="1.0.1",
     author="Oscar Beijbom",
     author_email="oscar.beijbom@gmail.com",
     description="Spatial image analysis with caffe and pytorch backends.",
@@ -19,4 +19,17 @@ setuptools.setup(
         "Operating System :: OS Independent",
     ],
     python_requires='==3.5.*',
+    install_requires=[
+        'boto=2.49.0',
+        'wget=3.2',
+        'scikit-learn=0.17.1',
+        'scikit-image=0.15.0',
+        'scipy=0.19.1',
+        'numpy=1.17.1',
+        'Pillow==6.2.0',
+        'tqdm==4.43.0',
+        'fire==0.2.1',
+        'torch==1.4.0',
+        'torchvision==0.5.0'
+    ]
 )
