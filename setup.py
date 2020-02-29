11a654b2b3ced7c4ebe2f54465b47d0bb09363c1
Oscar Beijbom <oscar.beijbom@gmail.com>
2020-02-28 21:04:25 -0800

    fixed setup.py requirements

--- setup.py
+++ setup.py
@@ -4,15 +4,15 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="pyspacer-BEIJBOMM",  # Replace with your own username
-    version="1.0.1",
+    name="pyspacer",  # Replace with your own username
+    version="0.0.1",
     author="Oscar Beijbom",
     author_email="oscar.beijbom@gmail.com",
     description="Spatial image analysis with caffe and pytorch backends.",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/beijbom/pyspacer",
-    packages=setuptools.find_packages(),
+    packages=setuptools.find_packages(exclude=['scripts']),
     classifiers=[
         "Programming Language :: Python :: 3.5",
         "License :: OSI Approved :: MIT License",
@@ -20,14 +20,14 @@ setuptools.setup(
     ],
     python_requires='>=3.5, <3.7',
     install_requires=[
-        'boto=2.49.0',
-        'wget=3.2',
+        'boto==2.49.0',
+        'wget==3.2',
         'tqdm==4.43.0',
         'Pillow==6.2.0',
-        'numpy=1.17.5',
-        'scipy=1.2.3',
-        'scikit-learn=0.17.1',
-        'scikit-image=0.15.0',
+        'numpy==1.17.5',
+        'scipy==1.2.3',
+        'scikit-learn==0.17.1',
+        'scikit-image==0.15.0',
         'torch==1.4.0',
         'torchvision==0.5.0'
     ]
