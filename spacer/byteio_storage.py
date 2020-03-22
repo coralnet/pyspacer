90a95f49a5ab3ca3d8663c96b6dcb40110b53fb8
Oscar Beijbom <oscar.beijbom@gmail.com>
2020-03-22 14:20:27 -0700

    wip

--- spacer/byteio_storage.py
+++ spacer/byteio_storage.py
@@ -1,5 +1,9 @@
 import abc
 
+from io import BytesIO
+
+from spacer import config
+
 from spacer.data_classes import DataLocation
 
 
@@ -8,4 +12,25 @@ class Storage(abc.ABC):  # pragma: no cover
     @abc.abstractmethod
     def load(self, loc: DataLocation) -> BytesIO:
         pass
+
+    @abc.abstractmethod
+    def store(self, loc: DataLocation, content: BytesIO):
+        pass
+
+
+class S3Storage(Storage):
+
+    def load(self, loc):
+
+        conn = config.get_s3_conn()
+        bucket = conn.get_bucket(loc.bucketname)
+        key = bucket.get_key(loc.key)
+        return BytesIO(key.get_content_as_string())
+
+    def store(self, loc, stream: BytesIO):
+        conn = config.get_s3_conn()
+        bucket = conn.get_bucket(loc.bucketname)
+        key = bucket.new_key(loc.key)
+        key.set_content_from_file(stream)
+
 
---
+++ spacer/tests/test_byteio_storage.py
@@ -0,0 +1,34 @@
+import unittest
+import warnings
+from spacer import config
+
+from spacer.byteio_storage import S3Storage
+
+from spacer.data_classes import ImageFeatures, DataLocation
+
+
+@unittest.skipUnless(config.HAS_S3_TEST_ACCESS, 'No access to test bucket')
+class TestS3Storage(unittest.TestCase):
+
+    def setUp(self):
+        warnings.simplefilter("ignore", ResourceWarning)
+        self.tmp_image_key = 'tmp_image.jpg'
+        self.tmp_json_key = 'tmp_data.json'
+        self.tmp_model_key = 'tmp_model.pkl'
+
+        conn = config.get_s3_conn()
+        self.bucket = conn.get_bucket('spacer-test')
+
+    def tearDown(self):
+        self.bucket.delete_key(self.tmp_image_key)
+        self.bucket.delete_key(self.tmp_json_key)
+        self.bucket.delete_key(self.tmp_model_key)
+
+    def test_image_features(self):
+
+        feats = ImageFeatures.example()
+        loc = DataLocation(storage_type='s3',
+                                     bucket_name='spacer-test',
+                                     key=self.tmp_json_key)
+        S3Storage.store(
+                        )
