1651cfe5b6cb3529e20b16fcf17bde1878a9c4b9
qimin <liam951120@gmail.com>
2020-04-16 20:48:57 -0700

    specify num_classes of the model

--- spacer/models/__init__.py
+++ spacer/models/__init__.py
@@ -4,9 +4,9 @@ For possible future network extension
 from .efficientnet import EfficientNet
 
 
-def get_model(model_type, model_name, num_classes=1000):
+def get_model(model_type, model_name, num_classes):
     if model_type == 'efficientnet':
-        model = EfficientNet.from_pretrained(model_name, num_classes)
+        model = EfficientNet.from_pretrained(model_name, num_classes=num_classes)
     else:
         raise NotImplementedError(model_name)
     return model
