d1af04d43045535a3318d32e660c34a0625cbc95
qimin <liam951120@gmail.com>
2020-04-16 18:41:26 -0700

    add classify_from_patchlist()

--- spacer/torch_utils.py
+++ spacer/torch_utils.py
@@ -2,14 +2,20 @@
 This file contains a set of pytorch utility functions
 """
 
-import PIL
 import torch
 import models
 import numpy as np
-from PIL import Image
-from typing import List, Tuple
 from torchvision import transforms
 from collections import OrderedDict
+
+
+def gray2rgb(im):
+    w, h = im.shape
+    ret = np.empty((w, h, 3), dtype=np.uint8)
+    ret[:, :, 0] = im
+    ret[:, :, 1] = im
+    ret[:, :, 2] = im
+    return ret
 
 
 def transformation():
@@ -65,9 +71,60 @@ def crop_simple(im, center, crop_size):
     return im[upper: upper + crop_size, left: left + crop_size, :]
 
 
-def classify_from_patchlist(im_pil: Image,
-                            point_anns: List[Tuple[int, int, int]],
-                            model_name: str,
-                            modelweighs_path: str):
+def load_weights(model, modelweighs_path):
+    """
+    Load model weights, original weight saved with DataParallel
+    Create new OrderedDict that does not contain `module`.
+    :param model: Currently support EfficientNet
+    :param modelweighs_path: pretrained model weight from new CoralNet
+    :return: well trained model
+    """
+    state_dicts = torch.load(modelweighs_path, map_location=torch.device('cpu'))
+    new_state_dicts = OrderedDict()
+    for k, v in state_dicts['net'].items():
+        name = k[7:]
+        new_state_dicts[name] = v
+    model.load_state_dict(new_state_dicts)
+    for param in model.parameters():
+        param.requires_grad = False
+    return model
+
+
+def classify_from_patchlist(im_pil, point_anns, pyparams):
+    """
+    Crop patches and extract features
+    :param im_pil: Image
+    :param point_anns: List[Tuple[int, int, int]] -> List[Tuple(row, col, label)]
+    :param pyparams: parameter dict
+    :return:
+    """
     # Model setup and load pretrained weight
-    net = models.get_model('efficientnet', 'efficientnet-b0')
+    net = models.get_model(pyparams['model_type'], pyparams['model_name'])
+    net = load_weights(net, pyparams['weights_path'])
+
+    scale = 1
+    feats_list, gtlist = [], []
+    transformer = transformation()
+
+    # Convert to numpy (call 2 times needed otherwise get a
+    # "IndexError: tuple index out of range" error. No idea why!
+    _ = np.asarray(im_pil)
+    im = np.asarray(im_pil)
+
+    if len(im.shape) == 2 or im.shape[2] == 1:
+        im = gray2rgb(im)
+    im = im[:, :, :3]  # only keep the first three color channels
+
+    # Crop patches
+    patch_list, label_list = crop_patch(im, pyparams['crop_size'], scale, point_anns)
+    patch_list = torch.stack([transformer(i) for i in patch_list])
+
+    # Feed forward and extract features
+    num_batch = int(np.ceil(len(patch_list) / pyparams['batch_size']))
+    bs = pyparams['batch_size']
+    for b in range(num_batch):
+        current_batch = patch_list[b*bs: b*bs + min(len(patch_list[b*bs:]), bs)]
+        features = net.extract_features(current_batch)
+        feats_list.extend(features.tolist())
+
+    return label_list, feats_list
