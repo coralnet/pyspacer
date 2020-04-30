25ff96743d2aeff92be8fe9db6e53c473436ad30
Oscar Beijbom <oscar.beijbom@gmail.com>
2020-04-29 23:30:41 -0700

    minor updates

--- scripts/aws_shakeout.py
+++ scripts/aws_shakeout.py
@@ -3,27 +3,29 @@ from spacer import config
 import json
 
 
-def submit_simple():
-    msg = JobMsg(
-        task_name='extract_features',
-        tasks=[ExtractFeaturesMsg(
-            job_token='regression_job',
-            feature_extractor_name='vgg16_coralnet_ver1',
-            rowcols=[(10, 10)],
-            image_loc=DataLocation(storage_type='s3',
-                                   key='08bfc10v7t.png',
-                                   bucket_name='spacer-test'),
-            feature_loc=DataLocation(storage_type='s3',
-                                     key='tmp/08bfc10v7t.png.feats.json',
-                                     bucket_name='spacer-test')
-        )])
+def submit_simple(job_cnt):
+    for jobid in range(job_cnt):
+        msg = JobMsg(
+            task_name='extract_features',
+            tasks=[ExtractFeaturesMsg(
+                job_token='regression_job',
+                feature_extractor_name='vgg16_coralnet_ver1',
+                rowcols=[(10, 10)],
+                image_loc=DataLocation(storage_type='s3',
+                                       key='08bfc10v7t.png',
+                                       bucket_name='spacer-test'),
+                feature_loc=DataLocation(storage_type='s3',
+                                         key='tmp/08bfc10v7t.png.feats{}.json'.
+                                         format(jobid),
+                                         bucket_name='spacer-test')
+            )])
 
-    queue_name = 'spacer_test_jobs'
-    conn = config.get_sqs_conn()
-    in_queue = conn.get_queue(queue_name)
-    msg = in_queue.new_message(body=json.dumps(msg.serialize()))
-    in_queue.write(msg)
-    print('submitted')
+        queue_name = 'spacer_test_jobs'
+        conn = config.get_sqs_conn()
+        in_queue = conn.get_queue(queue_name)
+        msg = in_queue.new_message(body=json.dumps(msg.serialize()))
+        in_queue.write(msg)
+        print('submitted job {}'.format(jobid))
 
 
 def read_results():
@@ -38,9 +40,9 @@ def read_results():
         queue.delete_message(m)
         m = queue.read()
 
-    print('Done')
+    print('Done reading results queue.')
 
 
 if __name__ == '__main__':
     read_results()
-    submit_simple()
+    submit_simple(5)
