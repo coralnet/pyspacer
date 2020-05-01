23f4af3cc4cf0482b6a110d6a810a9616cfac30f
qimin <liam951120@gmail.com>
2020-04-30 17:47:48 -0700

    remove two unit test

--- spacer/tests/test_extract_features_utils.py
+++ spacer/tests/test_extract_features_utils.py
@@ -3,10 +3,6 @@ import unittest
 import numpy as np
 from PIL import Image
 
-from io import BytesIO
-from spacer import config
-from spacer.storage import load_image
-from spacer.messages import DataLocation
 from spacer.extract_features_utils import gray2rgb, crop_patch
 
 
@@ -44,46 +40,6 @@ class TestCropPatch(unittest.TestCase):
         self.assertEqual(len(patch_list), len(rowcols))
         self.assertEqual(patch_list[0].shape[0], crop_size)
         self.assertEqual(patch_list[0].shape[1], crop_size)
-
-    def test_real_image(self):
-        rowcols = [(20, 265),
-                   (76, 295),
-                   (59, 274),
-                   (151, 62),
-                   (265, 234)]
-        crop_size = 224
-        image_loc = DataLocation(storage_type='s3',
-                                 key='08bfc10v7t.png',
-                                 bucket_name='spacer-test')
-        img = load_image(image_loc)
-        patch_list = crop_patch(
-            im=img,
-            rowcols=rowcols,
-            crop_size=crop_size
-        )
-
-        conn = config.get_s3_conn()
-        bucket = conn.get_bucket('spacer-test', validate=True)
-        npy_key = bucket.get_key('legacy_cropped.npy')
-        legacy_npy = np.load(BytesIO(npy_key.get_contents_as_string()))
-
-        self.assertTrue(np.allclose(legacy_npy, np.array(patch_list)))
-
-
-class TestOpenImage(unittest.TestCase):
-
-    def test_open(self):
-        conn = config.get_s3_conn()
-        bucket = conn.get_bucket('spacer-test', validate=True)
-        npy_key = bucket.get_key('legacy_np.npy')
-        legacy_npy = np.load(BytesIO(npy_key.get_contents_as_string()))
-
-        image_loc = DataLocation(storage_type='s3',
-                                 key='08bfc10v7t.png',
-                                 bucket_name='spacer-test')
-        img_npy = np.array(load_image(image_loc))[:, :, :3]
-
-        self.assertTrue(np.allclose(legacy_npy, img_npy))
 
 
 if __name__ == '__main__':
