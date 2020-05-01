97ecd2378aaed4f1fc584a99f45d5a4e7e13eb5b
qimin <liam951120@gmail.com>
2020-04-30 17:01:45 -0700

    test cropping on real image

--- spacer/tests/test_extract_features_utils.py
+++ spacer/tests/test_extract_features_utils.py
@@ -45,6 +45,30 @@ class TestCropPatch(unittest.TestCase):
         self.assertEqual(patch_list[0].shape[0], crop_size)
         self.assertEqual(patch_list[0].shape[1], crop_size)
 
+    def test_real_image(self):
+        rowcols = [(20, 265),
+                   (76, 295),
+                   (59, 274),
+                   (151, 62),
+                   (265, 234)]
+        crop_size = 224
+        image_loc = DataLocation(storage_type='s3',
+                                 key='08bfc10v7t.png',
+                                 bucket_name='spacer-test')
+        img = load_image(image_loc)
+        patch_list = crop_patch(
+            im=img,
+            rowcols=rowcols,
+            crop_size=crop_size
+        )
+
+        conn = config.get_s3_conn()
+        bucket = conn.get_bucket('spacer-test', validate=True)
+        npy_key = bucket.get_key('legacy_cropped.npy')
+        legacy_npy = np.load(BytesIO(npy_key.get_contents_as_string()))
+
+        self.assertTrue(np.allclose(legacy_npy, np.array(patch_list)))
+
 
 class TestOpenImage(unittest.TestCase):
 
