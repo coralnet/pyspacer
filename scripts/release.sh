1a5edb24256bba60f9c6fdf793b0ed481fa670af
Oscar Beijbom <oscar.beijbom@gmail.com>
2021-05-03 22:07:53 -0700

    adding a release script; made weights public and simplified build

--- scripts/release.sh
+++ scripts/release.sh
@@ -3,5 +3,5 @@ ENV=${1:-staging}
 NAME=public.ecr.aws/w6h8w6v4/coralnet.spacer:${ENV}
 
 aws ecr-public get-login-password --region us-east-1 | docker login --username AWS --password-stdin public.ecr.aws
-docker build -t ${NAME} .
+docker build --platform linux/amd64 -t ${NAME} .
 docker push ${NAME}
