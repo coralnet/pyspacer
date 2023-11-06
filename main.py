d194e19dc8696467a7824b5468b4113f9b449c9d
Lauren <yee.laurene@gmail.com>
2023-11-06 18:49:37 -0500

    download from s3 and run script locally

--- main.py
+++ main.py
@@ -10,7 +10,8 @@ import json
 from operator import itemgetter
 import os
 import boto3
-import botocore.exceptions 
+import botocore.exceptions
+from botocore.exceptions import NoCredentialsError, ClientError
 from pathlib import Path
 from spacer import config
 from spacer.data_classes import ImageLabels
@@ -26,33 +27,43 @@ from spacer.tasks import classify_features, extract_features, train_classifier
 # Load the secret.json file
 with open('secrets.json', 'r') as f:
     secrets = json.load(f)
-
-aws_access_key_id = secrets['AWS_ACCESS_KEY_ID']
-aws_secret_access_key = secrets['AWS_SECRET_ACCESS_KEY']
-region = secrets['AWS_REGION']
-
 # Create an S3 resource using credentials from the secret.json file
 s3 = boto3.resource(
     's3',
-    region_name=region,
-    aws_access_key_id=aws_access_key_id,
-    aws_secret_access_key=aws_secret_access_key
+    region_name=secrets['AWS_REGION'],
+    aws_access_key_id=secrets['AWS_ACCESS_KEY_ID'],
+    aws_secret_access_key=secrets['AWS_SECRET_ACCESS_KEY']
 )
 
-bucket_name = 'pyspacer-test'  # The bucket you have read permissions for
+# The bucket you have read permissions for
+bucket_name = 'pyspacer-test'  
+# Create a bucket object
+bucket = s3.Bucket(bucket_name)
+try:
+    # List objects within the bucket
+    for obj in bucket.objects.all():
+        print(f'Object: {obj.key}')
+except botocore.exceptions.ClientError as e:
+    print(f'An error occurred: {e}')
+except botocore.exceptions.NoCredentialsError as e:
+    print("Credentials not available")
+
 # Module directory is local directory
 module_dir = Path.cwd()
 output_dir = module_dir / 'output'
 TOP_SCORES_PER_POINT = 5
 
-# Use S3 as the DataLocation and configure the AWS credentials
-# Use secrets.json to retrieve the AWS credentials
-# Use the AWS credentials to configure the DataLocation
-# data_loc = DataLocation(
-#     storage_type='s3',
-#     key='',
-#     bucket_name='pyspacer-test'
-# )
+# Create client
+
+s3_client = boto3.client(
+    's3',
+    region_name=secrets['AWS_REGION'],
+    aws_access_key_id=secrets['AWS_ACCESS_KEY_ID'],
+    aws_secret_access_key=secrets['AWS_SECRET_ACCESS_KEY']
+)
+
+# Download everything following the same folder structure in the bucket
+# Should add a test here for file size, user prompt, etc.
 
 def image_to_feature_filepath(image_filepath: Path) -> Path:
     return output_dir / 'features' / (image_filepath.stem + '.featurevector')
@@ -64,15 +75,34 @@ def image_filepath_to_id(image_filepath: Path) -> str:
 
 if __name__ == '__main__':
 
-    annotated_image_dir = Path(module_dir, 'images-annotated')
-    unannotated_image_dir = Path(module_dir, 'images-unannotated')
-    extractor_weights_filepath = Path(module_dir, 'efficientnet_b0_ver1.pt')
+    annotated_image_dir = module_dir / 'images-annotated'
+    unannotated_image_dir = module_dir / 'images-unannotated'
+    extractor_weights_filepath = module_dir / 'efficientnet_b0_ver1.pt'
 
-    with open(Path(module_dir, 'annotations.json')) as f:
+    # The local directory to which you want to download the files is the current working directory
+    local_directory = Path.cwd()
+
+    # List and download all objects from the bucket
+    paginator = s3_client.get_paginator('list_objects_v2')
+    for page in paginator.paginate(Bucket=bucket_name):
+        for obj in page.get('Contents', []):
+            # Define the local path where the file should be downloaded
+            local_file_path = local_directory / obj['Key']
+            local_file_dir = local_file_path.parent
+
+            # Create directories if they don't exist
+            os.makedirs(local_file_dir, exist_ok=True)
+
+            # Download the file
+            s3_client.download_file(bucket_name, obj['Key'], str(local_file_path))
+            print(f"Downloaded {obj['Key']} to {local_file_path}")
+
+    print("Download completed")
+    with open(module_dir / 'annotations.json') as f:
         all_annotations = json.load(f)
 
     label_ids_to_codes = dict()
-    with open(Path(module_dir, 'labelset.csv')) as f:
+    with open(module_dir / 'labelset.csv') as f:
         reader = csv.reader(f)
         for label_id, label_code in reader:
             label_ids_to_codes[label_id] = label_code
@@ -101,12 +131,12 @@ if __name__ == '__main__':
             extractor=EfficientNetExtractor(
                 data_locations=dict(
                     weights=DataLocation(
-                        's3', str(extractor_weights_filepath)),
+                        'filesystem', str(extractor_weights_filepath)),
                 ),
             ),
             rowcols=[(row, col) for row, col, _ in annotations],
-            image_loc=DataLocation('s3', str(image_filepath)),
-            feature_loc=DataLocation('s3', str(feature_filepath)),
+            image_loc=DataLocation('filesystem', str(image_filepath)),
+            feature_loc=DataLocation('filesystem', str(feature_filepath)),
         )
         return_message = extract_features(message)
         print(
@@ -130,10 +160,9 @@ if __name__ == '__main__':
         annotations = all_annotations[image_id]
         data[feature_filepath] = annotations
 
-    output_dir = Path('pyspacer-test/output')
-    classifier_filepath = output_dir / Path('classifier1.pkl')
-    valresult_filepath = output_dir / Path('valresult.json')
+    classifier_filepath = output_dir / 'classifier1.pkl'
     valresult_filepath = output_dir / 'valresult.json'
+
     message = TrainClassifierMsg(
         job_token='classifier1',
         trainer_name='minibatch',
